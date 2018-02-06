e97a703372006df71798e2e779aeee920954b7f8
Joeri van Dooren <ure@moreorless.be>
2018-02-06 22:16:31 +0100

    fix

--- aprs-tracker/app/mojo.pl
+++ aprs-tracker/app/mojo.pl
@@ -166,7 +166,7 @@ post '/aprs-tracker/:token/feed/enco_io' => sub {
     my $appuser = $self->auth;
     return unless $appuser;
 
-    my $data  = $c->req->params->to_hash;
+    my $data  = $self->req->params->to_hash;
 
     $self->log(Dumper $data);
     
